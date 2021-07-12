ed0378796eb11694adf831d5699094015cec82da
Brandon Eads <brandon181989@hotmail.com>
2021-07-12 16:31:29 -0700

    clean up code
    Co-authored-by: Ben Pawlik <james.benjamin.pawlik@gmail.com>
    Co-authored-by: Michael Reiersgaard <michael.reiersgaard@gmail.com>

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,8 +1,6 @@
 class Array
   def queen_attack?(prey)
-    if (self[0] - prey[0]).abs() != (self[1]-prey[1]).abs() && 
-      self[0]!= prey[0] &&
-      self[1] != prey[1]
+    if (self[0] - prey[0]).abs() != (self[1]-prey[1]).abs() && self[0]!= prey[0] && self[1] != prey[1]
       false
     else true
     end
