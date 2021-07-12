3e445a0d4cd1388f764cf6d130fc3073bdbc8ee3
Brandon Eads <brandon181989@hotmail.com>
2021-07-12 16:04:57 -0700

    pass first tests, prev commit only 3 tests

--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -6,7 +6,13 @@ describe('Array#queen_attack') do
   it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
     expect(queen_attack([1,1],[2,3])).to(eq(false))
   end
-  it('is true if the coordinates are  horizontally, vertically, or diagonally in line with each other') do
+  it('is true if the coordinates are diagonally in line with each other') do
     expect(queen_attack([1,1],[4,4])).to(eq(true))
+  end
+  it('is true if the coordinates are horizontally in line with each other') do
+    expect(queen_attack([1,1],[1,4])).to(eq(true))
+  end
+  it('is true if the coordinates are vertically in line with each other') do
+    expect(queen_attack([1,1],[4,1])).to(eq(true))
   end
 end
